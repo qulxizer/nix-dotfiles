482a1bbfa31cde64d2830018f2e90076d82156de
mahdi <mahdi@1jz.cc>
2026-08-08 00:19:51 +0300

    fix: rewrite

--- nixos-modules/programs/gpg.nix
+++ nixos-modules/programs/gpg.nix
@@ -2,6 +2,6 @@
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true; # Optional: replaces ssh-agent with gpg-agent
-    pinentryPackage = "curses"; # Or "qt" / "curses" depending on your desktop environment
+    pinentryPackage = "qt"; # Or "qt" / "curses" depending on your desktop environment
   };
 }
