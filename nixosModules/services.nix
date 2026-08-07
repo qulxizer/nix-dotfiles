243a8e71bf765fa562ba457f76b87b524d64e6a7
mahdi <mahdi@1jz.cc>
2026-08-07 14:02:59 +0300

    fix: rewrite

--- nixosModules/services.nix
+++ nixosModules/services.nix
@@ -1,19 +1,21 @@
 { ... }: {
-
 services.keyd = {
   enable = true;
-  settings = {
-    main = {
-      # Add your device IDs here ('*' applies to all keyboards)
-      ids = [ "*" ];
-      # Define your custom key mappings in the main layer
-      main = {
-        # Example 1: Remap Caps Lock to Escape
-        capslock = "esc";
-        
-        # Example 2: Remap Right Alt to Right Control
-        rightalt = "rightcontrol";
+  keyboards = {
+    # The name is just the name of the configuration file, it does not really matter
+    default = {
+      ids = [ "*" ]; # what goes into the [id] section, here we select all keyboards
+      # Everything but the ID section:
+      settings = {
+        # The main layer, if you choose to declare it in Nix
+        main = {
+          capslock = "esc"; # you might need to also enclose the key in quotes if it contains non-alphabetical symbols
+        };
+        otherlayer = {};
       };
+      extraConfig = ''
+        # put here any extra-config, e.g. you can copy/paste here directly a configuration, just remove the ids part
+      '';
     };
   };
 };
