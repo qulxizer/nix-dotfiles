9e08172e2fc58a84bb7d86f0a16e2a6f83e99329
mahdi <mahdi@1jz.cc>
2026-08-07 14:48:02 +0300

    fix: rewrite

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
               useGlobalPkgs = true;
               useUserPackages = true;
               extraSpecialArgs = { inherit inputs; }
-              users.mahdi = 
+              users."${username}" = 
                   hosts/workstation/home.nix;
             };
           }
