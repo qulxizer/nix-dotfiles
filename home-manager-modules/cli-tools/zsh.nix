f721ce3b88dc48f6b68fc1f1511f27dfa669f66e
mahdi <mahdi@1jz.cc>
2026-08-08 01:48:05 +0300

    add: foot

--- home-manager-modules/cli-tools/zsh.nix
+++ home-manager-modules/cli-tools/zsh.nix
@@ -1,8 +1,9 @@
 { pkgs, config, ... }:
 {
+  home.file.".p10k.zsh".source = ../../non-home-manager/zsh/.p10k.zsh;
   programs.zsh = {
     enable = true;
-    initExtra = ''
+    initContent = ''
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
     zplug = {
