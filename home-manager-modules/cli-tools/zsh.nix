da35953c48872e926ab02d859f9f235d9668c38b
mahdi <mahdi@1jz.cc>
2026-08-08 01:58:00 +0300

    add: foot

--- home-manager-modules/cli-tools/zsh.nix
+++ home-manager-modules/cli-tools/zsh.nix
@@ -1,25 +1,28 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}:
-{
+{ pkgs, lib, ... }: {
   home.file.".p10k.zsh".source = ../../non-home-manager/zsh/.p10k.zsh;
 
   programs.zsh = {
     enable = true;
 
-    # Loads p10k instant prompt if you use it (optional but recommended)
-    initExtraFirst = ''
-      if [[ -r "\''${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-\''${(%):-%n}.zsh" ]]; then
-        source "\''${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-\''${(%):-%n}.zsh"
-      fi
-    '';
+    # Actually load the theme engine
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
 
-    # Actually sources your config
-    initExtra = ''
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-    '';
+    # Modern way to init in HM, fixes your warnings
+    initContent = lib.mkMerge [
+      (lib.mkBefore ''
+        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+        fi
+      '')
+      ''
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      ''
+    ];
   };
 }
