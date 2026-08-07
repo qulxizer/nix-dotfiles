623b9e73d268ca17b7d026b7725641c523149af6
mahdi <mahdi@1jz.cc>
2026-08-08 02:46:17 +0300

    add: foot

--- home-manager-modules/programs/niri.nix
+++ home-manager-modules/programs/niri.nix
@@ -1,22 +1,76 @@
 { pkgs, ... }: {
   programs.niri = {
-    # package = pkgs.niri;
     enable = true;
 
     settings = {
+      prefer-no-csd = true;
+      screenshot-path = "~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png";
+
       environment = {
         QT_QPA_PLATFORM = "wayland";
         DISPLAY = ":0";
       };
 
       spawn-at-startup = [
+        { command = [ "/usr/lib/polkit-kde-authentication-agent-1" ]; }
+        {
+          command = [
+            "bash"
+            "-c"
+            "wl-paste --watch cliphist store"
+          ];
+        }
         { command = [ "waybar" ]; }
+        { command = [ "awww-daemon" ]; }
+        {
+          command = [
+            "bash"
+            "-c"
+            "awww img ~/nix-dotfiles/wallpapers/musashi.png"
+          ];
+        }
         { command = [ "dunst" ]; }
+        { command = [ "xwayland-satellite" ]; }
       ];
+
+      input = {
+        keyboard.xkb = { };
+        touchpad = {
+          tap = true;
+          natural-scroll = true;
+        };
+        mouse.accel-profile = "flat";
+        trackpoint = { };
+      };
+
+      outputs."DP-1" = {
+        mode = "1920x1080@240";
+        transform = "normal";
+        position = {
+          x = 1280;
+          y = 0;
+        };
+      };
 
       layout = {
         gaps = 10;
         center-focused-column = "never";
+        default-column-width.proportion = 0.5;
+
+        preset-column-widths = [
+          { proportion = 0.25; }
+          { proportion = 0.5; }
+          { proportion = 0.75; }
+          { proportion = 1.0; }
+        ];
+        preset-window-heights = [
+          { proportion = 0.25; }
+          { proportion = 0.5; }
+          { proportion = 0.75; }
+          { proportion = 1.0; }
+        ];
+
+        focus-ring.off = true;
         border = {
           width = 3;
           active.color = "#cba6f7";
@@ -24,23 +78,42 @@
         };
       };
 
+      window-rules = [
+        {
+          matches = [ { app-id = "com-st-microxplorer-maingui-STM32CubeMX"; } ];
+          default-column-width.proportion = 0.6;
+        }
+        {
+          geometry-corner-radius = 8;
+          clip-to-geometry = true;
+        }
+        {
+          matches = [
+            {
+              app-id = "firefox$";
+              title = "^Picture-in-Picture$";
+            }
+          ];
+          open-floating = true;
+        }
+        {
+          matches = [ { app-id = "ghostty$"; } ];
+          opacity = 0.9;
+        }
+      ];
+
       binds = {
+        "Mod+Shift+Slash".action.show-hotkey-overlay = { };
+
         "Mod+Return".action.spawn = [ "ghostty" ];
         "Mod+D".action.spawn = [
-          "rofi"
-          "-show"
-          "drun"
+          "bash"
+          "-c"
+          "~/.config/rofi/launchers/type-1/launcher.sh"
         ];
+        "Mod+B".action.spawn = [ "brave" ];
+        "Super+Alt+L".action.spawn = [ "swaylock" ];
 
-        "Mod+Q".action.close-window = { };
-        "Mod+Shift+E".action.quit = { };
-
-        "Mod+Left".action.focus-column-left = { };
-        "Mod+Right".action.focus-column-right = { };
-        "Mod+Up".action.focus-window-up = { };
-        "Mod+Down".action.focus-window-down = { };
-
-        # The audio binds are way cleaner now
         "XF86AudioRaiseVolume" = {
           allow-when-locked = true;
           action.spawn = [
@@ -50,6 +123,165 @@
             "0.1+"
           ];
         };
+        "XF86AudioLowerVolume" = {
+          allow-when-locked = true;
+          action.spawn = [
+            "wpctl"
+            "set-volume"
+            "@DEFAULT_AUDIO_SINK@"
+            "0.1-"
+          ];
+        };
+        "XF86AudioMute" = {
+          allow-when-locked = true;
+          action.spawn = [
+            "wpctl"
+            "set-mute"
+            "@DEFAULT_AUDIO_SINK@"
+            "toggle"
+          ];
+        };
+        "XF86AudioMicMute" = {
+          allow-when-locked = true;
+          action.spawn = [
+            "wpctl"
+            "set-mute"
+            "@DEFAULT_AUDIO_SOURCE@"
+            "toggle"
+          ];
+        };
+
+        "Mod+Q".action.close-window = { };
+
+        "Mod+Left".action.focus-column-left = { };
+        "Mod+Down".action.focus-window-down = { };
+        "Mod+Up".action.focus-window-up = { };
+        "Mod+Right".action.focus-column-right = { };
+        "Mod+H".action.focus-column-left = { };
+        "Mod+J".action.focus-window-down = { };
+        "Mod+K".action.focus-window-up = { };
+        "Mod+L".action.focus-column-right = { };
+
+        "Mod+Ctrl+Left".action.move-column-left = { };
+        "Mod+Ctrl+Down".action.move-window-down = { };
+        "Mod+Ctrl+Up".action.move-window-up = { };
+        "Mod+Ctrl+Right".action.move-column-right = { };
+        "Mod+Ctrl+H".action.move-column-left = { };
+        "Mod+Ctrl+J".action.move-window-down = { };
+        "Mod+Ctrl+K".action.move-window-up = { };
+        "Mod+Ctrl+L".action.move-column-right = { };
+
+        "Mod+Home".action.focus-column-first = { };
+        "Mod+End".action.focus-column-last = { };
+        "Mod+Ctrl+Home".action.move-column-to-first = { };
+        "Mod+Ctrl+End".action.move-column-to-last = { };
+
+        "Mod+Shift+Left".action.focus-monitor-left = { };
+        "Mod+Shift+Down".action.focus-monitor-down = { };
+        "Mod+Shift+Up".action.focus-monitor-up = { };
+        "Mod+Shift+Right".action.focus-monitor-right = { };
+        "Mod+Shift+H".action.focus-monitor-left = { };
+        "Mod+Shift+J".action.focus-monitor-down = { };
+        "Mod+Shift+K".action.focus-monitor-up = { };
+        "Mod+Shift+L".action.focus-monitor-right = { };
+
+        "Mod+Shift+Ctrl+Left".action.move-column-to-monitor-left = { };
+        "Mod+Shift+Ctrl+Down".action.move-column-to-monitor-down = { };
+        "Mod+Shift+Ctrl+Up".action.move-column-to-monitor-up = { };
+        "Mod+Shift+Ctrl+Right".action.move-column-to-monitor-right = { };
+        "Mod+Shift+Ctrl+H".action.move-column-to-monitor-left = { };
+        "Mod+Shift+Ctrl+J".action.move-column-to-monitor-down = { };
+        "Mod+Shift+Ctrl+K".action.move-column-to-monitor-up = { };
+        "Mod+Shift+Ctrl+L".action.move-column-to-monitor-right = { };
+
+        "Mod+Page_Down".action.focus-workspace-down = { };
+        "Mod+Page_Up".action.focus-workspace-up = { };
+        "Mod+U".action.focus-workspace-down = { };
+        "Mod+I".action.focus-workspace-up = { };
+
+        "Mod+Ctrl+Page_Down".action.move-column-to-workspace-down = { };
+        "Mod+Ctrl+Page_Up".action.move-column-to-workspace-up = { };
+        "Mod+Ctrl+U".action.move-column-to-workspace-down = { };
+        "Mod+Ctrl+I".action.move-column-to-workspace-up = { };
+
+        "Mod+Shift+Page_Down".action.move-workspace-down = { };
+        "Mod+Shift+Page_Up".action.move-workspace-up = { };
+        "Mod+Shift+U".action.move-workspace-down = { };
+        "Mod+Shift+I".action.move-workspace-up = { };
+
+        "Mod+WheelScrollDown" = {
+          cooldown-ms = 150;
+          action.focus-workspace-down = { };
+        };
+        "Mod+WheelScrollUp" = {
+          cooldown-ms = 150;
+          action.focus-workspace-up = { };
+        };
+        "Mod+Ctrl+WheelScrollDown" = {
+          cooldown-ms = 150;
+          action.move-column-to-workspace-down = { };
+        };
+        "Mod+Ctrl+WheelScrollUp" = {
+          cooldown-ms = 150;
+          action.move-column-to-workspace-up = { };
+        };
+
+        "Mod+WheelScrollRight".action.focus-column-right = { };
+        "Mod+WheelScrollLeft".action.focus-column-left = { };
+        "Mod+Ctrl+WheelScrollRight".action.move-column-right = { };
+        "Mod+Ctrl+WheelScrollLeft".action.move-column-left = { };
+        "Mod+Shift+WheelScrollDown".action.focus-column-right = { };
+        "Mod+Shift+WheelScrollUp".action.focus-column-left = { };
+        "Mod+Ctrl+Shift+WheelScrollDown".action.move-column-right = { };
+        "Mod+Ctrl+Shift+WheelScrollUp".action.move-column-left = { };
+
+        "Mod+1".action.focus-workspace = 1;
+        "Mod+2".action.focus-workspace = 2;
+        "Mod+3".action.focus-workspace = 3;
+        "Mod+4".action.focus-workspace = 4;
+        "Mod+5".action.focus-workspace = 5;
+        "Mod+6".action.focus-workspace = 6;
+        "Mod+7".action.focus-workspace = 7;
+        "Mod+8".action.focus-workspace = 8;
+        "Mod+9".action.focus-workspace = 9;
+
+        "Mod+Ctrl+1".action.move-column-to-workspace = 1;
+        "Mod+Ctrl+2".action.move-column-to-workspace = 2;
+        "Mod+Ctrl+3".action.move-column-to-workspace = 3;
+        "Mod+Ctrl+4".action.move-column-to-workspace = 4;
+        "Mod+Ctrl+5".action.move-column-to-workspace = 5;
+        "Mod+Ctrl+6".action.move-column-to-workspace = 6;
+        "Mod+Ctrl+7".action.move-column-to-workspace = 7;
+        "Mod+Ctrl+8".action.move-column-to-workspace = 8;
+        "Mod+Ctrl+9".action.move-column-to-workspace = 9;
+
+        "Mod+BracketLeft".action.consume-or-expel-window-left = { };
+        "Mod+BracketRight".action.consume-or-expel-window-right = { };
+        "Mod+Comma".action.consume-window-into-column = { };
+        "Mod+Period".action.expel-window-from-column = { };
+
+        "Mod+R".action.switch-preset-column-width = { };
+        "Mod+Shift+R".action.switch-preset-window-height = { };
+        "Mod+Ctrl+R".action.reset-window-height = { };
+        "Mod+F".action.maximize-column = { };
+        "Mod+Shift+F".action.fullscreen-window = { };
+        "Mod+C".action.center-column = { };
+
+        "Mod+Minus".action.set-column-width = "-10%";
+        "Mod+Equal".action.set-column-width = "+10%";
+        "Mod+Shift+Minus".action.set-window-height = "-10%";
+        "Mod+Shift+Equal".action.set-window-height = "+10%";
+
+        "Mod+V".action.toggle-window-floating = { };
+        "Mod+Shift+V".action.switch-focus-between-floating-and-tiling = { };
+
+        "Print".action.screenshot = { };
+        "Ctrl+Print".action.screenshot-screen = { };
+        "Alt+Print".action.screenshot-window = { };
+
+        "Mod+Shift+E".action.quit = { };
+        "Ctrl+Alt+Delete".action.quit = { };
+        "Mod+Shift+P".action.power-off-monitors = { };
       };
     };
   };
