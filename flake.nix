03d09ff321b63cac0878c23c52d4a5a24aa2725e
mahdi <mahdi@1jz.cc>
2026-08-07 14:48:41 +0300

    fix: rewrite

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
-              extraSpecialArgs = { inherit inputs; }
+              extraSpecialArgs = { inherit inputs; };
               users."${username}" = hosts/workstation/home.nix;
             };
           }
