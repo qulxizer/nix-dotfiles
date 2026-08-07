797a6d09dba0a7a9f13c0ce6fd37ae4daf04822d
mahdi <mahdi@1jz.cc>
2026-08-08 01:49:37 +0300

    add: foot

--- home-manager-modules/cli-tools/zsh.nix
+++ home-manager-modules/cli-tools/zsh.nix
@@ -1,11 +1,25 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   home.file.".p10k.zsh".source = ../../non-home-manager/zsh/.p10k.zsh;
+
   programs.zsh = {
     enable = true;
-    initContent = ''
+
+    initExtraFirst = ''
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
+
+    initExtraBeforeCompInit = ''
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
+
     zplug = {
       enable = true;
       plugins = [
