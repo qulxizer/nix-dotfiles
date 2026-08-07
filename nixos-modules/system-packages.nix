6910038de3de485c0c4bf5b2d433a3b314906243
mahdi <mahdi@1jz.cc>
2026-08-08 00:38:01 +0300

    add: python

--- nixos-modules/system-packages.nix
+++ nixos-modules/system-packages.nix
@@ -1,6 +1,7 @@
 { pkgs, inputs, ... }: {
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    python3
     wget
     swaylock
     fuzzel
