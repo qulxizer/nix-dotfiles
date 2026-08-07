9177aed5dc0cac5e7c81827cd41d0bc03c897644
mahdi <mahdi@1jz.cc>
2026-08-08 01:56:23 +0300

    add: foot

--- home-manager-modules/cli-tools/zsh.nix
+++ home-manager-modules/cli-tools/zsh.nix
@@ -10,28 +10,16 @@
   programs.zsh = {
     enable = true;
 
+    # Loads p10k instant prompt if you use it (optional but recommended)
     initExtraFirst = ''
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      if [[ -r "\''${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-\''${(%):-%n}.zsh" ]]; then
+        source "\''${XDG_CACHE_HOME:-\$HOME/.cache}/p10k-instant-prompt-\''${(%):-%n}.zsh"
       fi
     '';
 
-    initExtraBeforeCompInit = ''
+    # Actually sources your config
+    initExtra = ''
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
-
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-        {
-          name = "romkatv/powerlevel10k";
-          tags = [
-            "as:theme"
-            "depth:1"
-          ];
-        }
-      ];
-    };
   };
 }
