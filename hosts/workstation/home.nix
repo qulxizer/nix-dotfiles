1f0ecd51fe9c43eb3eb37397736e67f1735e900b
mahdi <mahdi@1jz.cc>
2026-08-07 15:03:11 +0300

    fix: rewrite

--- hosts/workstation/home.nix
+++ hosts/workstation/home.nix
@@ -1,15 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      # ./../../homeManagerModules/programs/hyprland.nix
-      ./../../home-manager-modules/cli-tools/git.nix
-      # ./../../homeManagerModules/programs/hyprpaper.nix
-      # ./../../homeManagerModules/programs/alacritty.nix
-      # ./../../home-manager-modules/cli-tools/zsh.nix
+  imports = [
+    # ./../../homeManagerModules/programs/hyprland.nix
+    ./../../home-manager-modules/cli-tools/git.nix
+    # ./../../homeManagerModules/programs/hyprpaper.nix
+    # ./../../homeManagerModules/programs/alacritty.nix
+    ./../../home-manager-modules/cli-tools/zsh.nix
 
-    ];
+  ];
 
   home.username = "mahdi";
   home.homeDirectory = "/home/mahdi";
@@ -31,7 +30,6 @@
         "x-scheme-handler/unknown" = "${browser}";
       };
   };
-
 
   home.stateVersion = "25.05";
 }
