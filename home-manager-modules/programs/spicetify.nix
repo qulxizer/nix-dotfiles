2bd3efa02cecc88555039e353982a3a4b3e07e1c
mahdi <mahdi@1jz.cc>
2026-08-07 23:44:52 +0300

    fix: rewrite

---
+++ home-manager-modules/programs/spicetify.nix
@@ -0,0 +1,16 @@
+{ pkgs, inputs, ... }:
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in
+{
+  programs.spicetify = {
+    enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+      adblockify
+      hidePodcasts
+      shuffle
+    ];
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
+  };
+}
