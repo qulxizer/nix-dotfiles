44e5bf523a71e965f37b0197a49ccfbddbcdaf5e
mahdi <mahdi@1jz.cc>
2026-08-08 02:49:53 +0300

    add: foot

--- home-manager-modules/programs/niri.nix
+++ home-manager-modules/programs/niri.nix
@@ -58,7 +58,7 @@
 
       layout = {
         gaps = 10;
-        center-focused-column = "never";
+        # center-focused-column = "never";
         default-column-width.proportion = 0.5;
 
         preset-column-widths = [
