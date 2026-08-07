fd43e66c46594618a5d26383b3318b7cb965cfbb
mahdi <mahdi@1jz.cc>
2026-08-08 02:49:14 +0300

    add: foot

--- home-manager-modules/programs/niri.nix
+++ home-manager-modules/programs/niri.nix
@@ -48,8 +48,8 @@
       };
 
       outputs."DP-1" = {
-        mode = "1920x1080@240";
-        transform = "normal";
+        # mode = "1920x1080@240";
+        # transform = "normal";
         position = {
           x = 1280;
           y = 0;
