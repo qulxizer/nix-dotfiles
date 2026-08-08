652a0c8c2ec2c0078044fbd6854403ae31aa7074
mahdi <mahdi@1jz.cc>
2026-08-08 05:42:55 +0300

    add: controller support

--- nixos-modules/programs/steam.nix
+++ nixos-modules/programs/steam.nix
@@ -2,6 +2,13 @@
   programs.xwayland.enable = true;
   environment.systemPackages = with pkgs; [
     xwayland-satellite
+    SDL2
+  ];
+  hardware.steam-hardware.enable = true;
+  hardware.uinput.enable = true;
+
+  services.udev.packages = with pkgs; [
+    game-devices-udev-rules
   ];
   # environment.systemPackages = pkgs.xwayland-satellite;
   programs.steam.enable = true;
